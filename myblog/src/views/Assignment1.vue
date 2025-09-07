36cac6f66ebc95b86bd80ddab3a0bca93bf8650f
noodlix <zhibeck271205@gmail.com>
2025-09-07 23:35:41 +0500

    huijhy

--- myblog/src/views/Assignment1.vue
+++ myblog/src/views/Assignment1.vue
@@ -67,16 +67,43 @@
 
     
     <div class="text-block fade-slide">
-        
+        The visual mode and aural mode of the music video are strikingly contrasting - ambient sounds play as character of Theo with a bruised face full of blood stares tiredly into distance right before energetic pop song starts as the character angrily smashes the horn and cries in frustration as it was discussed in Writer Designer chapter 1, the video showcases multimodality and provokes intense emotions from the audience juxtaposing the calm and injured opening with the next intense scene, making it more impactful than either mode could achieve on its own. 
+The music video is edited so that it plays out in reverse, being presented nonlinearly, which as it is said in Writer Designer chapter 3 adds a dynamic dimension to the music video, the person watching has to figure out why Theo’s character is in make-up and beat up car-crashing into people, piecing together the chronology from visual cues.
+
     </div>
 
     
     <!-- <div class="img-wrapper">
-    5  <img src="/meopic.JPG" alt="Hurts Band" class="styled-img" />
+    6  <img src="/meopic.JPG" alt="Hurts Band" class="styled-img" />
     </div> -->
 
     
     <div class="text-block fade-slide">
+      As the video progresses, we go back in time to the three men undressing Theo’s character in a dull parking lot, and as it was mentioned in The Media Insider’s video on semiotics, editing is one of the four pillars of polisemics, along with Mise-en-scene, camerawork and sound. As the lyrics say, “put on your coat” and “make them all desperate to be you” violent undressing and assault are seen as men dressing the main character up, putting him into white high heels, black sequin, red leather vest and long dark hair which symbolize hyperfemininity, exaggeration and camp, just like the character had put the items on at home before going out, stepping into the drag queens persona and experimenting with gender, flowing from one extreme into another and back.
+    </div>
+        
+    <!-- <div class="img-wrapper">
+    7 <img src="/meopic.JPG" alt="Hurts Band" class="styled-img" />
+    </div> -->
+
+        <!-- <div class="img-wrapper">
+    8  <img src="/meopic.JPG" alt="Hurts Band" class="styled-img" />
+    </div> -->
+    
+    <div class="text-block fade-slide">
+As the video goes back in time more, Theo’s character showcases excellent acting skills, going from laid-back, gum chewing, relaxed person who just got out of the club to get some air, still lip-syncing to music and spinning around, portraying gender euphoria and connection to the person's true self, going to careful, sensing danger, hyper aware of his surroundings, scaredly adjusting the top to cover up and trying to shield themselves, realizing his own vulnerability, the music video uses the gestural mode, body language and facial expressions to symbolize a shift in tone, and then the character starts full on running away from the three men, trying to escape and save himself from perceived transphobic and homophobic predators.
+    </div>
+
+        <div class="text-block fade-slide">
+          The next scene is of the main character in the club’s neon pink lights, surrounded by posters with the words “club lento” and a stylized merge of trans and rainbow flags which are made to look like the progress pride flag, with each color representing life, healing, sunlight, nature, serenity, spirit, people of colour and the HIV/AIDS community, and those who are living with or have died from HIV/AIDS, stigma surrounding the virus, transgender and non-binary people and the arrow shape emphasizing progress according to Women’s College hospital. As it was mentioned in Gee’s Language and learning, language was always multimodal, combining words, images, and sound, and it is up to the audience’s interpretation what colors and shapes mean.
+
+
+
+The hair and make up are canonically drag, with high arched eyebrows which mask masculine high eyebrow bridge, bold, eye enlarging eyeshadow that goes down to undereyes, big eyelash extensions, tons of blush, perfect tone that covers up skin texture and facial hair, sculpting nose and cheec contour which feminize the face and big, overlined lips are present, along with wide diameter hoop earrings and voluminous, long hair.
+Here are some examples of canonical drag queen makeup, popularized on RuPauls Drag race, from which some drag makeup trends originate, such as Trixie Mattels’s unblended line contour and Katya Zamolodchikova’s russian doll aesthetic.
+
+
+In the next scene we see Theo’s character enjoying the nightlife, dancing with other LGBTQ people, undisturbed and feeling in the right place, as one of the three perpetrators of assault is drinking at the table on the sidelines having a good distance between them, eyes transfixed on him, gaze following his every move across the club, as the audience wonders the reasoning, before it is finally revealed. The man attacking the main character has shot his shot and was rejected by the drag queen, making the themes of the music video even more unsettling and brutal. The band Hurts shouts out a theme relevant as ever, men feeling entitled to the object of their desire so hard that the idea of being rejected sends them spiralling, making them stalk and physically assault the poot soul that failed to reciprocate their feelings, wounding their fragile ego. In 2017, the year the music video was produced,  killings of transgender people hit record high, according to the headline of lvcriminaldefense.
     </div>
 
   </div>
